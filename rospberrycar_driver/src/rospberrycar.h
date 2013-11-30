331fa0229d81b6170e87f796cdfe62fafc72fd93
Joao Sousa <sousa1989joao@gmail.com>
2013-11-30 13:04:47 +0000

    typedef

--- rospberrycar_driver/src/rospberrycar.h
+++ rospberrycar_driver/src/rospberrycar.h
@@ -10,6 +10,24 @@ typedef enum
     FAST_DECAY_COAST = 0, SLOW_DECAY_BRAKE
 }HBridgeDriveMode;
 
+typedef struct ROSCASDataToSTELLARIS_
+{
+    int8_t v_linear;
+    int8_t v_angular;
+    
+    uint8_t cmd;		// 0b <blinky_bit> <ask_data_bit> <HBridgeDriveMode> <0> <0> <0> <0> <start_stop_car_bit>
+}ROSCASDataToSTELLARIS;
+
+typedef struct ROSCASDataFromSTELLARIS_
+{
+    uint8_t battery_voltage;
+    uint8_t battery_current;
+    int32_t left_encoder_count;
+    int32_t right_encoder_count;
+    
+}ROSCASDataFromSTELLARIS;
+
+
 class ROSpberryCar
 {
 public:
@@ -19,6 +37,8 @@ public:
     bool initialize();
     
     void drive(int linear_velocity, int angular_velocity, HBridgeDriveMode mode = FAST_DECAY_COAST);
+    
+    bool getStellarisData(void);
     
     bool isReady(){ return car_ready_; }
     
